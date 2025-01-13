a84ce92970e3632bd5e4ae42de324272e0fb3c78
N4TIXE <nattapolice12@gmail.com>
2025-01-13 20:54:09 +0700

    Add files via upload

---
+++ Nattapon 509-14.py
@@ -0,0 +1,26 @@
+print("EX.1")
+i = 1
+while i <= 21:
+    print(i)
+    i += 2
+print("\nEX.2")
+i = 2
+while i <= 17:
+    print(i)
+    i += 3
+print("\nEX.3")
+i = 100
+while i >= -100:
+    print(i)
+    i -= 10
+print("\nEX.4")
+i = -30
+while i <= 30:
+    print(i)
+    i += 10
+print("\nEX.5")
+i = 15
+while i <= 55:
+    print(i)
+    i += 8
+
